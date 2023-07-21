2f10a7a58e294a768baeac14afbc3e525c05bc27
Sean Ghods <137848705+seanghods@users.noreply.github.com>
2023-07-20 20:09:59 -0700

    Add basic HTML, CSS styling, JS functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+function operate(firstNumber, secondNumber, operator) {
+    return firstNumber operator secondNumber;
+}
